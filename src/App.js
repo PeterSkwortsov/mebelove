bfbd6ff9e9b1162947b6699f8549d9cc54a9b784
Peter <skwortsow.petia@yandex.ru>
2024-10-02 22:21:12 +0300

    Первое кресло

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.scss";
-//Components
+import { Canvas, useFrame } from "@react-three/fiber";
 import Header from "./components/header";
+import { Html, useGLTF } from "@react-three/drei";
+import { Section } from "./components/section";
+import { useRef } from "react";
 
 export default function App() {
+
+  const Model = () => {
+    const gltf = useGLTF("/armchairYellow.gltf", true);
+    return <primitive object={gltf.scene} dispose={null}/>;
+  }
+
+  const Lights = () => (
+    <>
+      <ambientLight intensity={1.0} />
+      <directionalLight position={[10, 10, 7]} intensity={4.5} />
+    </>
+  );
+
+  
+
+  const HTMLContent = () => (
+
+
+  
+
+    <Section factor={1.5} offset={1}>
+      <group position={[0, 250, 0]}>
+        <mesh position={[0, -35, 0]} >
+          <Model />
+        </mesh>
+        <Html fullscreen>
+          <div className="container">
+            <h1 className="title">Hello</h1>
+          </div>
+        </Html>
+      </group>
+    </Section>
+  );
+
+
   return (
     <>
       <Header />
-      <div className='container'>
-        <div className='title'>
-          <h1>Stater Branch</h1>
-        </div>
-      </div>
+      <Canvas
+        colorManagement
+        camera={{ position: [0, 0, 120], fov: 70 }}> 
+        <Lights />
+        <Suspense fallback={null}>
+          <HTMLContent />
+        </Suspense>
+      </Canvas>
     </>
   );
 }
