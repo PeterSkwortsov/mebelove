acc025e0251b7e7b3337b74907a9710320c77763
Peter <skwortsow.petia@yandex.ru>
2024-10-02 22:57:50 +0300

    Вторая модель, прокрутка

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 import Header from "./components/header";
 import { Html, useGLTF } from "@react-three/drei";
 import { Section } from "./components/section";
 import { useRef } from "react";
+import state from "./components/state";
 
-export default function App() {
 
-  const Model = () => {
-    const gltf = useGLTF("/armchairYellow.gltf", true);
+
+  const Model = ({ modelPath }) => {
+    const gltf = useGLTF(modelPath, true);
     return <primitive object={gltf.scene} dispose={null}/>;
   }
 
@@ -22,24 +23,32 @@ export default function App() {
 
   
 
-  const HTMLContent = () => (
+const HTMLContent = ({ domContent, children, modelPath, positionY}) => {
 
+    const ref = useRef();
+    useFrame(() => (ref.current.rotation.y += 0.005));
 
-  
+   return (
+     <> 
+     <Section factor={1.5} offset={1}>
+         <group position={[0, positionY, 0]}>
+         <mesh position={[0, -35, 0]} ref={ref}>
+             <Model modelPath={modelPath} />
+         </mesh>
+         <Html portal={domContent} fullscreen>
+          {children}
+         </Html>
+       </group>
+     </Section>
+    </>
+   )
+  }
 
-    <Section factor={1.5} offset={1}>
-      <group position={[0, 250, 0]}>
-        <mesh position={[0, -35, 0]} >
-          <Model />
-        </mesh>
-        <Html fullscreen>
-          <div className="container">
-            <h1 className="title">Hello</h1>
-          </div>
-        </Html>
-      </group>
-    </Section>
-  );
+export default function App() {
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({target: scrollArea.current}), [])
 
 
   return (
@@ -50,9 +59,28 @@ export default function App() {
         camera={{ position: [0, 0, 120], fov: 70 }}> 
         <Lights />
         <Suspense fallback={null}>
-          <HTMLContent />
+
+          <HTMLContent modelPath="/armchairYellow.gltf" positionY={250}
+          domContent={domContent}
+          >
+            <div className="container">
+              <h1 className="title">Желтый</h1>
+            </div>
+          </HTMLContent>
+          <HTMLContent modelPath="/armchairGreen.gltf" positionY={0}
+            domContent={domContent}>
+            <div className="container">
+              <h1 className="title">Зеленый</h1>
+            </div>
+          </HTMLContent>
+
+
         </Suspense>
       </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{ position: 'sticky', top: 0}} ref= {domContent}></div>
+        <div style={{height: `${state.pages * 100}vh`}}></div>
+      </div>
     </>
   );
 }
